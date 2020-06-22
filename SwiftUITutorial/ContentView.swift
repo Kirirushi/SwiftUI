a8ec61c7aa7b5cd98e3148386e59adb335d0ea8a
Игровой Kirirushi <GameKirirushi@Kirirushi.local>
2020-06-22 15:19:51 +0300

    Initialization

---
+++ SwiftUITutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorial
+//
+//  Created by Игровой Kirirushi on 22.06.2020.
+//  Copyright © 2020 Kirirushi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
